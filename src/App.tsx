ee9b0d8fe848e45242da457a319e699acfced63e
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-11-02 10:31:14 -0300

    Create project react with bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-primary'>
+        testeeeeeee
+      </div>      
+    </div>
+  );
+}
+
+export default App;
