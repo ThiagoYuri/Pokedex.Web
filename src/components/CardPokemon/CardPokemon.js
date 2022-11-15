605a7485d611e0367d1c8961a6fef0988258f936
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-11-15 14:52:15 -0300

    Create components CardPokemon and InfoPokemon

---
+++ src/components/CardPokemon/CardPokemon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import styles from './CardPokemon.module.scss';
+
+const CardPokemon = () => (
+  <div className={styles.CardPokemon}>
+    CardPokemon Component
+  </div>
+);
+
+CardPokemon.propTypes = {};
+
+CardPokemon.defaultProps = {};
+
+export default CardPokemon;
---
+++ src/components/InfoPokemon/InfoPokemon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import styles from './InfoPokemon.module.scss';
+
+const InfoPokemon = () => (
+  <div className={styles.InfoPokemon}>
+    InfoPokemon Component
+  </div>
+);
+
+InfoPokemon.propTypes = {};
+
+InfoPokemon.defaultProps = {};
+
+export default InfoPokemon;
