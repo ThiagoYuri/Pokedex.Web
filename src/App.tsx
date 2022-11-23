990291b7900c43e23e9fdcf78cd1ab4b9891ba18
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-11-22 22:13:23 -0300

    Create style cardPokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import CardPokemon from './components/CardPokemon/CardPokemon.js';
 
 function App() {
   return (
     <div className="App">
-      <div className='bg-primary'>
-        testeeeeeee
+      <div className='container'>
+        <div className="row">
+          <div className="col-12 col-lg-3 col-sm-6" style={{ paddingTop: '10px' }}>
+            <CardPokemon/>
+          </div>
+        </div>      
       </div>      
     </div>
   );
--- src/components/CardPokemon/CardPokemon.js
+++ src/components/CardPokemon/CardPokemon.js
@@ -1,10 +1,19 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 import styles from './CardPokemon.module.scss';
+import gifLoading from '../../resources/loading.gif'
 
 const CardPokemon = () => (
   <div className={styles.CardPokemon}>
-    CardPokemon Component
+      <div className="card border border-primary rounded" id={styles.CardPokemon}>
+        <img className="card-img-top bg-secondary" src={gifLoading} alt="Loading" id={styles.imageDirectory} ></img>
+        <div className="card-body">
+          <p className="card-text text-black" style={{textAlign: "center"}}>Name Pokemon</p>
+        </div>
+        <div className="card-footer">
+        <p  class="btn btn-secondary btn-lg disabled" aria-disabled="true" >types</p>
+        </div>
+      </div>
   </div>
 );
 
