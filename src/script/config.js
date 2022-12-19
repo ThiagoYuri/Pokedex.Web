ae2ce6ccd0559c1c1caca0cd2b78246db534e49c
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-12-19 11:09:35 -0300

    add: URLs pokeAPI

---
+++ src/script/config.js
@@ -0,0 +1,6 @@
+
+config={
+    getListPokemons:"https://pokeapi.co/api/v2/pokemon?limit={0}&offset={1}",// {0} is limit and {1} is position
+    getPokemonInfo: "https://pokeapi.co/api/v2/pokemon/{0}/", // {0} is pokemon name or id 
+    getImagePokemon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{0}.png", // {0} is pokemon ID
+}
