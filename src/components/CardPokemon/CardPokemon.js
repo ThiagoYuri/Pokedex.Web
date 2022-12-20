bbaed07890921c7dc2e084378d61289fcef3fc87
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-12-20 08:33:41 -0300

    Update: card styles

--- src/components/CardPokemon/CardPokemon.js
+++ src/components/CardPokemon/CardPokemon.js
@@ -4,12 +4,18 @@ import styles from './CardPokemon.module.scss';
 import gifLoading from '../../resources/loading.gif'
 
 const CardPokemon = () => (
-  <div className={styles.CardPokemon}>
-      <div className="card border border-primary rounded" id={styles.CardPokemon}>
-        <img className="card-img-top bg-secondary" src={gifLoading} alt="Loading" id={styles.imageDirectory} ></img>
-        <div className="card-body">
-          <p className="card-text text-black" style={{textAlign: "center"}}>Name Pokemon</p>
-        </div>
+  <div>
+      <div className="card border border-primary rounded" >
+        <div id={styles.fundoCard}> 
+          <div>
+            <div id={styles.fundoImagePokemon}>
+              <img className="card-img-top" src={gifLoading} alt="Loading"></img>
+            </div>
+          </div>
+          <div className="card-body">
+            <p className="card-text text-black" style={{textAlign: "center"}}>Name Pokemon</p>
+          </div>
+        </div>       
         <div className="card-footer">
         <p  class="btn btn-secondary btn-lg disabled" aria-disabled="true" >types</p>
         </div>
